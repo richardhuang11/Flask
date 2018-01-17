02747d4638a1e655bafecd59fa133b9be6a12d23
richardhuang11 <richardhuang11@gmail.com>
2018-01-17 14:18:55 -0800

    added migrations, updated sql

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class BaseConfig(object):
 	SECRET_KEY = '\x99\x83J2\xc4\xe0\xadc\x06\xf4\xd3%\x7f\xb3[\xaf\x87\xd1=\xb8\xb2a\xb3\xb9'
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
-
+	print(SQLALCHEMY_DATABASE_URI)
 
 class DevelopmentConfig(BaseConfig):
 	DEBUG = True
--- db_create.py
+++ db_create.py
@@ -10,6 +10,10 @@ db.session.add(BlogPost("Good", "I\'m good."))
 db.session.add(BlogPost("Excellent", "I\'m excellent."))
 db.session.add(BlogPost("Okay", "I\'m okay."))
 db.session.add(BlogPost("Bad", "I\'m bad."))
+db.session.add(BlogPost("Flask", "discoverflask.com"))
+db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
+
+
 
 #commit the changes
 db.session.commit()
